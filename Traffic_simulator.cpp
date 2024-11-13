38d0118d693d9e623c6605c5ae30777ed440e1fe
rautsagar214 <rautsagar214@gmail.com>
2024-11-13 12:10:14 +0530

    abstract class and virtual function

--- Traffic_simulator.cpp
+++ Traffic_simulator.cpp
@@ -10,22 +10,15 @@ enum class LightState {
     GREEN
 };
 
-// Base Class Vehicle
+// Abstract Base Class Vehicle
 class Vehicle {
 public:
-    Vehicle() : name("Unnamed"), roadIndex(0) {}
     Vehicle(const string& name, int roadIndex)
         : name(name), roadIndex(roadIndex) {}
 
-    // Virtual function for polymorphism
-    virtual void move() const {
-        cout << setw(15) << left << name << " is moving on road " << roadIndex + 1 << ".\n";
-    }
-
-    // Virtual function for polymorphism
-    virtual void stop() const {
-        cout << setw(15) << left << name << " has stopped on road " << roadIndex + 1 << ".\n";
-    }
+    // Pure virtual functions (Abstract methods)
+    virtual void move() const = 0;  // Must be overridden by derived classes
+    virtual void stop() const = 0;  // Must be overridden by derived classes
     
     string getName() const { return name; }
     int getRoadIndex() const { return roadIndex; }
@@ -41,15 +34,14 @@ public:
     EmergencyVehicle(const string& name, int roadIndex)
         : Vehicle(name, roadIndex) {}
 
-    // Overriding move method for emergency vehicles (Polymorphism)
+    // Overriding move method for emergency vehicles
     void move() const override {
         cout << setw(15) << left << name << " (EMERGENCY) is moving on road " << roadIndex + 1 << ".\n";
     }
 
-    // Overriding stop method for emergency vehicles (Polymorphism)
+    // Overriding stop method for emergency vehicles
     void stop() const override {
-        cout << setw(15
-        ) << left << name << " is an emergency vehicle and doesn't stop.\n";
+        cout << setw(15) << left << name << " is an emergency vehicle and doesn't stop.\n";
     }
 };
 
@@ -62,12 +54,10 @@ public:
         trafficLights.resize(numRoads);
     }
 
-    // Polymorphism: addVehicle can work with different types of vehicles
     virtual void addVehicle(Vehicle* vehicle) {
         vehicles.push_back(vehicle);
     }
 
-    // Polymorphism: run can call different move/stop methods based on vehicle type
     virtual void run() {
         int cycles;
         cout << "Enter the number of cycles: ";
@@ -77,7 +67,6 @@ public:
             cout << "Cycle " << i + 1 << "\n";
             trafficLights[i % numRoads] = LightState::GREEN;
 
-            // Polymorphism: Correct method (move/stop) is called based on vehicle type
             for (Vehicle* v : vehicles) {
                 if (trafficLights[v->getRoadIndex()] == LightState::GREEN) {
                     v->move();
@@ -100,7 +89,6 @@ public:
     AdvancedTrafficSimulation(int numVehicles, int numRoads)
         : TrafficSimulation(numVehicles, numRoads) {}
 
-    // Overriding the run method (Polymorphism)
     void run() override {
         int cycles;
         cout << "Enter the number of advanced simulation cycles: ";
@@ -110,9 +98,8 @@ public:
             cout << "Advanced Cycle " << i + 1 << "\n";
             trafficLights[i % numRoads] = LightState::GREEN;
 
-            // Polymorphism: Correct move method is called based on vehicle type
             for (Vehicle* v : vehicles) {
-                v->move();
+                v->move();  // Every vehicle just moves in the advanced simulation
             }
         }
     }
@@ -126,10 +113,8 @@ int main() {
     cout << "Enter number of vehicles: ";
     cin >> numVehicles;
 
-    // Advanced simulation
     AdvancedTrafficSimulation sim(numVehicles, numRoads);
 
-    // Adding vehicles to the simulation
     for (int i = 0; i < numVehicles; ++i) {
         string name;
         int roadIndex;
@@ -141,7 +126,6 @@ int main() {
         cout << "Is it an emergency vehicle? (1 for Yes, 0 for No): ";
         cin >> isEmergency;
 
-        // Polymorphism: Different types of vehicles added to simulation
         if (isEmergency) {
             sim.addVehicle(new EmergencyVehicle(name, roadIndex));
         } else {
@@ -149,7 +133,6 @@ int main() {
         }
     }
 
-    // Running the simulation
     sim.run();
     return 0;
 }
